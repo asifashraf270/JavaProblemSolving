81e5d3d6033842ca923ed8b81d2e268c9f1d1095
Asif <muhammadasifashraf270@gmail.com>
2022-04-20 10:50:51 +0400

    MultipleDimensional Array

---
+++ src/com/androiddevelopoer/Arrays/MultiDimensionalArray.java
@@ -0,0 +1,30 @@
+package com.androiddevelopoer.Arrays;
+
+public class MultiDimensionalArray {
+    private int[][] array={{1,2,3},{4,5,6},{7,8,9}};
+    public void display2DArrayData()
+    {
+        for (int i=0;i<3;i++)
+        {
+            for (int j=0;j<3;j++)
+            {
+                System.out.println(array[i][j]+".....record");
+            }
+        }
+    }
+
+    public void sum(int[] arr)
+    {
+        int sum=0;
+        for (int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+        }
+        System.out.println("Sum is:"+sum);
+    }
+    public int[] returnArray()
+    {
+        return new int[]{1,2,3};
+    }
+
+}
--- src/com/androiddevelopoer/Main.java
+++ src/com/androiddevelopoer/Main.java
@@ -1,9 +1,22 @@
 package com.androiddevelopoer;
 
+import com.androiddevelopoer.Arrays.MultiDimensionalArray;
+import sun.rmi.runtime.Log;
+
 public class Main {
+    private static final String TAG = "Main";
+    public static MultiDimensionalArray multiDimensionalArray;
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+        // write your code here
+        multiDimensionalArray=new MultiDimensionalArray();
+        multiDimensionalArray.display2DArrayData();
+        int[] sumArray={1,2,3,4};
+        multiDimensionalArray.sum(sumArray);
+        int[] returnedArray=multiDimensionalArray.returnArray();
+        for (int i=0;i<returnedArray.length;i++)
+        {
+            System.out.println(TAG+returnedArray[i]);
+        }
     }
 }
